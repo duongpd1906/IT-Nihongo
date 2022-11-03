05aa4a9dc4378b9153d68b6bafb4637653cfea3b
Nguyen Anh Tuan <91835187+hades154@users.noreply.github.com>
2022-11-03 13:56:44 +0700

    login

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <route path="/" element={<Login />} />
+        <Route path="/" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
