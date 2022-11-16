13fdd125d728009b01f69fa68a717ef3306db888
Bao Ng <baonn207201@gmail.com>
2022-11-16 20:08:54 +0700

    Hompage includes ListTopic and ListTopic includes Topic

---
+++ client/src/pages/Home/Topic.js
@@ -0,0 +1,42 @@
+import Card from 'react-bootstrap/Card';
+import "./Topic.css";
+import Form from 'react-bootstrap/Form';
+
+const Topic = (props) => {
+    const {src, name} = props;
+    return(
+        <div className="topic">
+            <Card className="card_style" style={{ width: '18rem', height:'18rem' }}>
+                <Card.Img className="card_img" variant="top" src={src} />
+
+                <Card.Body style={{ textAlign: 'center', backgroundColor: 'transparent' }}>
+                    <Card.Title>{name}</Card.Title>
+                    <Card.Text>
+                        <Form>
+                            {['radio'].map((type) => (
+                                <div key={`inline-${type}`} className="mb-3">
+                                <Form.Check
+                                    inline
+                                    label="X"
+                                    name="group1"
+                                    type={type}
+                                    id={`inline-${type}-X`}
+                                />
+                                <Form.Check
+                                    inline
+                                    label="O"
+                                    name="group1"
+                                    type={type}
+                                    id={`inline-${type}-O`}
+                                />
+                                </div>
+                            ))}
+                        </Form>
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </div>
+    );
+}
+
+export default Topic;
