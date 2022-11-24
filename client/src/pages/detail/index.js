b8680b92cd8f93f9711f3e7bd373a090c54b42a9
nhientnd <nhientnd@smartosc.com>
2022-11-24 09:06:18 +0700

    css/detail-topics-sidebar - Fix list topic css - Fix sidebar css

--- client/src/pages/detail/index.js
+++ client/src/pages/detail/index.js
@@ -1,60 +1,42 @@
-import './stylect.css';
-import imgg from '../../assets/img/down.jpg';
-import logoo from '../../assets/img/logoo.png';
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowCircleLeft } from '@fortawesome/free-solid-svg-icons'
-
-
+import './Detail.css';
 
 function Detail() {
 
     return (
-        <div className="Appct">
-            {/* <h1>Hello chi tiet </h1> */}
-            <div className="header">
-                <ul className="chung">
-                    <li><img className="logoo" src={logoo} /></li>
-                    <li><h1 id="dn">ログインは<a href="/login">こちら</a></h1></li>
-                </ul> 
-            </div>
-
-            {/* <FontAwesomeIcon icon={faArrowLeft} /> */}
-
-            <div className="slider">
-                <div className="labell1">
-                    {/* <div className="btntv">--</div> */}
-                    <Link to="/">
-                        {/* <input className="btntv" type="button" value={faArrowLeft} ></input> */}
-                        <FontAwesomeIcon className="btntv" icon={faArrowCircleLeft} />
-                    </Link>
-                    <label>詳細</label><br></br>
-                </div>
-                <div className="labell2">
-                    <label>デザイン</label>
-                </div>
-            </div>
-
-            <form className="form">
-                <img className="img" src={imgg} />
-                <div className="input-container">
-                    <label>額</label><br></br>
-                    <input type="text" name="額" className="input" required />
-                </div>
-                <div className="input-container">
-                    <label>位置</label><br></br>
-                    <input type="text" name="位置" className="input" required />
-                </div>
-
-                <div className="input-container">
-                    <label>説明</label><br></br>
-                    <input type="text" name="説明" className="input" required />
-
-                </div>
-                <button type="submit" className="btn">確認</button>
-
-            </form>
+        <div className="detail-page">
+            HEllo
         </div>
+
+        // <div className="Appct">
+        //     <div className="slider">
+        //         <div className="labell1">
+        //             <label>詳細</label><br></br>
+        //         </div>
+        //         <div className="labell2">
+        //             <label>デザイン</label>
+        //         </div>
+        //     </div>
+
+        //     <form className="form">
+        //         <img className="img" src='../../assets/img/down.jpg' alt="detail" />
+        //         <div className="input-container">
+        //             <label>額</label><br></br>
+        //             <input type="text" name="額" className="input" required />
+        //         </div>
+        //         <div className="input-container">
+        //             <label>位置</label><br></br>
+        //             <input type="text" name="位置" className="input" required />
+        //         </div>
+
+        //         <div className="input-container">
+        //             <label>説明</label><br></br>
+        //             <input type="text" name="説明" className="input" required />
+
+        //         </div>
+        //         <button type="submit" className="btn">確認</button>
+
+        //     </form>
+        // </div>
     );
 
 }
