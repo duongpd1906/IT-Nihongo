33b0485425d32756d899d22c2cc5b90b6d077bfb
Nguyen Anh Tuan <91835187+hades154@users.noreply.github.com>
2022-11-24 11:05:04 +0700

    admindetail

--- client/src/App.js
+++ client/src/App.js
@@ -6,19 +6,21 @@ import Error from "./pages/Error/Error";
 import Detail from "./pages/detail";
 import UpLoad from "./pages/upLoad/upLoad";
 import DesignChosen from "./pages/designPage/Design";
+import AdminDetailVote from "./pages/adminDetailVote/AdminDetail";
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-      <Route path="/design" element={<DesignChosen />} />
-      <Route path="/Upload" element={<UpLoad />} />
+        <Route path="/admindetail" element={<AdminDetailVote />} />
+        <Route path="/design" element={<DesignChosen />} />
+        <Route path="/Upload" element={<UpLoad />} />
         <Route path="/error" element={<Error />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/detail" element={<Detail />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<AdminDetailVote />} />
       </Routes>
     </BrowserRouter>
   );
